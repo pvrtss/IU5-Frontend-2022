edf2c2199b6e442d34ecc4b5344c933fa410984f
pvrtss <pvrtss@gmail.com>
2022-04-04 13:45:14 +0300

    tasks fix

--- lab6/script.js
+++ lab6/script.js
@@ -32,5 +32,87 @@ secondTask.addEventListener("mouseleave", () => {
 });
 
 /////////////////// Task 3 ///////////////////
+const thirdTask = document.getElementById("dropdown-button");
+const dropdown = document.getElementById("dropdown-content");
 
+thirdTask.addEventListener("click", () => {dropdown.classList.toggle("dropdown-content-show")});
 
+/////////////////// Task 4 ///////////////////
+let escFlag = true;
+const field = document.getElementById("field");
+const pacman = document.getElementById("pacman");
+document.addEventListener('mousemove', (mouse) => {
+	if (!escFlag) {
+		let x, y;
+		const minX = field.offsetLeft,
+		maxX = field.offsetLeft + field.offsetWidth - pacman.offsetWidth,
+		minY = field.offsetTop,
+		maxY = field.offsetTop + field.offsetHeight - pacman.offsetHeight;
+		const mouseX = mouse.pageX - pacman.offsetWidth / 2,
+		mouseY = mouse.pageY - pacman.offsetHeight / 2;
+
+		 if (mouseX < minX) x = 0;
+		 else if (mouseX > maxX) x = field.offsetWidth - pacman.offsetWidth - 2;
+		 else x = mouseX - minX;
+
+		 if (mouseY < minY) y = 0;
+		 else if (mouseY > maxY) y = field.offsetHeight - pacman.offsetHeight - 2;
+		 else y = mouseY - minY;
+
+		 pacman.style.left = `${x}px`;
+		 pacman.style.top = `${y}px`;
+	}
+ });
+
+ pacman.addEventListener('click', () => {
+	escFlag = false;
+ })
+
+ document.addEventListener('keyup', (k) => {
+ if (k.key.toLowerCase() === 'escape') {
+	escFlag = true;
+ }
+ })
+ 
+ /////////////////// Task 5 ///////////////////
+ const episodesButton = document.getElementById("episodes-button");
+ const episodes = document.getElementById("episodes")
+ 
+ episodesButton.addEventListener("click", () => {
+	fetch("https://breakingbadapi.com/api/episodes").then((data) => data.json()).then((data) => {
+		data.forEach((ep) => {
+			const episode = document.createElement("div");
+			episode.classList.add("episode-card");
+			
+			const title = document.createElement('h1');
+			title.innerText = `${ep.title}`
+			episode.appendChild(title);
+			
+			const season = document.createElement('h4');
+			season.innerText = `Сезон ${ep.season}`
+			episode.appendChild(season);
+			
+			const epNum = document.createElement('h4');
+			epNum.innerText = `Серия ${ep.episode}`
+			episode.appendChild(epNum);
+			
+			const airDate = document.createElement('h4');
+			airDate.innerText = `Дата выхода: ${ep.air_date}`
+			episode.appendChild(airDate);
+			
+			const chars = document.createElement('h4');
+			chars.innerText = "Персонажи:"
+			episode.appendChild(chars);
+			const characters = document.createElement('div');
+			characters.classList.add("characters");
+			ep.characters.forEach((ch) => {
+				const charact = document.createElement('p');
+				charact.innerText = ch;
+				characters.appendChild(charact);
+			}) 
+			episode.appendChild(characters);
+			episodes.appendChild(episode);
+		})
+	})
+ 
+ })
