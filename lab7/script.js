8c3d8c95b7021ac043cd1aba9e13967e44ba7058
pvrtss <pvrtss@gmail.com>
2022-04-11 02:07:41 +0300

    background fix

--- lab7/script.js
+++ lab7/script.js
@@ -1,3 +1,37 @@
+let api_key = "32273e1edabe3f7e12571409dbace3cf";
+
+function setBackground(str) {
+    switch (str.slice(0, 2)) {
+        case "01":
+            document.getElementById("weather-box").style.backgroundImage = "url('src/sunny.png')";
+            break;
+        case "02":
+            document.getElementById("weather-box").style.backgroundImage = "url('src/few-clouds.png')";
+            break;
+        case "03":
+            document.getElementById("weather-box").style.backgroundImage = "url('src/cloudy.png')";
+            break;
+        case "04":
+            document.getElementById("weather-box").style.backgroundImage = "url('src/more-cloudy.png')";
+            break;
+        case "09":
+            document.getElementById("weather-box").style.backgroundImage = "url('src/more-rainy.jpg')";
+            break;
+        case "10":
+            document.getElementById("weather-box").style.backgroundImage = "url('src/rainy.png')";
+            break;
+        case "11":
+            document.getElementById("weather-box").style.backgroundImage = "url('src/thunder.png')";
+            break;
+        case "13":
+            document.getElementById("weather-box").style.backgroundImage = "url('src/snow.png')";
+            break;
+        case "50":
+            document.getElementById("weather-box").style.backgroundImage = "url('src/mist.png')";
+            break;
+    }
+}
+
 function loadCurrentWeather(data) {
     document.getElementById("city-name").innerHTML = data.name;
     document.getElementById("temp").innerHTML =
@@ -35,24 +69,21 @@ function loadHourForecast(data) {
     }
 }
 
-function main() {
+function loadWeather(url, urlForecast) {
     let errorBox = document.getElementById("error-box");
     let errorMsg = document.getElementById("error-text");
-    let input = document.getElementById("search_input");
-    let city = input.value === "" ? "Москва" : encodeURIComponent(input.value);
-    let api_key = "32273e1edabe3f7e12571409dbace3cf";
-    let url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&lang=ru&units=metric&appid=" + api_key;
-    let urlForecast =
-        "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&lang=ru&units=metric&appid=" + api_key;
 
     fetch(url)
-        .then(response => response.json(), (error) => {
-            errorMsg.innerHTML = "<b>Ошибка!</b>" + error;
-            if (!errorBox.classList.contains("error-box-visible")) {
-                errorBox.classList.toggle("error-box-visible");
-                errorBox.classList.toggle("error-box-hidden");
+        .then(
+            (response) => response.json(),
+            (error) => {
+                errorMsg.innerHTML = "<b>Ошибка!</b>" + error;
+                if (!errorBox.classList.contains("error-box-visible")) {
+                    errorBox.classList.toggle("error-box-visible");
+                    errorBox.classList.toggle("error-box-hidden");
+                }
             }
-        })
+        )
         .then((result) => {
             if (result.cod === "404") {
                 errorMsg.innerHTML = "<b>Ошибка!</b>Город не найден. Попробуйте еще раз.";
@@ -67,9 +98,44 @@ function main() {
                 }
 
                 loadCurrentWeather(result);
+                setBackground(result.weather[0].icon);
                 fetch(urlForecast)
                     .then((response) => response.json())
                     .then((result) => loadHourForecast(result));
             }
         });
+}
+
+function loadWeatherFromInput() {
+    let input = document.getElementById("search_input");
+    let city = input.value === "" ? "Москва" : encodeURIComponent(input.value);
+
+    let url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&lang=ru&units=metric&appid=" + api_key;
+    let urlForecast =
+        "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&lang=ru&units=metric&appid=" + api_key;
+
+    loadWeather(url, urlForecast);
+}
+
+function main() {
+    loadWeatherFromInput();
+    const success = (position) => {
+        let url =
+            "https://api.openweathermap.org/data/2.5/weather?lat=" +
+            position.coords.latitude +
+            "&lon=" +
+            position.coords.longitude +
+            "&lang=ru&units=metric&appid=" +
+            api_key;
+        let urlForecast =
+            "https://api.openweathermap.org/data/2.5/forecast?lat=" +
+            position.coords.latitude +
+            "&lon=" +
+            position.coords.longitude +
+            "&lang=ru&units=metric&appid=" +
+            api_key;
+        loadWeather(url, urlForecast);
+    };
+
+    navigator.geolocation.getCurrentPosition(success);
 }
